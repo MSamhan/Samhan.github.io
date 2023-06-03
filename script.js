6e9e8258c16e3969229242877e370572655b0d02
Samhan-smj <68542691+Samhan-smj@users.noreply.github.com>
2023-06-03 20:37:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,55 @@
+
+                            /*Nav Bar SAid */
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => { 
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
+
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec =>{
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height){
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        };
+    });
+
+
+                            /* Stick navbar  */
+
+    let header = document.querySelector('header');
+
+    header.classList.toggle('sticky', window.scrollY > 100);
+
+
+                            /* Remove when click navbar */
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+
+};
+
+
+                                                            /* Scroll */
+
+    ScrollReveal({ 
+        reset: true ,
+        distannce: '80px',
+        duration: 2000,
+        delay: 200
+    });
+
+    ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+
+    
